f5e894f7c3d257a2749f914d1559e29a07da2607
S K <sylvain_kerjean@hotmail.com>
2016-03-29 15:23:53 +0200

    Adding some unit tests on proofs

---
+++ prop/test/test_proof_prop.ml
@@ -0,0 +1,20 @@
+open OUnit2
+open Formula_prop
+open Proof_prop
+
+let x1,x2 = PVar 1,PVar 2
+
+let test_invalid_empty_proof test_ctxt =
+        assert_raises ~msg:"Invalid empty proof" (Failure("Formula is not at the end of the proof")) (fun () -> proof_verification ~hyp:[] ~proof:[] x1)
+
+let test_invalid_proof test_ctxt =
+        assert_raises ~msg:"Invalid proof" (Invalid_demonstration (x1,[])) (fun () -> proof_verification ~hyp:[] ~proof:[x1] x1)
+
+let proof_prop_suite = "Proof prop" >:::
+        [ "test invalid empty proof">::test_invalid_empty_proof;
+          "test invalid proof">::test_invalid_proof;
+        ]
+
+
+let () = 
+        run_test_tt_main proof_prop_suite;
