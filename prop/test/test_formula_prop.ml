5cb573b865116ab3c16cbafab806d67c23ddbb2b
S K <sylvain_kerjean@hotmail.com>
2016-04-26 00:23:13 +0200

    Adapation of the test to the new dypgen parser. Unfinished.

--- prop/test/test_formula_prop.ml
+++ prop/test/test_formula_prop.ml
@@ -2,180 +2,184 @@ open OUnit2
 open Formula_prop
 open Axioms_prop
 open Proof_prop
+open Parser
 
 (* |- F=>.F *)
-let verif_tauto = (proof_verification ~hyp:[] (x1=>.x1) 
-~proof:[
-	(x1 =>.((x1 =>. x1)=>.x1)) =>.
-	(( x1 =>. (x1 =>. x1)) =>. (x1 =>. x1));
-	x1=>.((x1=>.x1)=>.x1);
-	(x1 =>. (x1 =>. x1)) =>. (x1 =>. x1);
-	x1=>.(x1=>.x1);
-	x1=>.x1
+let verif_tauto = proof_verification ~hyp:[] (formula_from_string "x1=>.x1") 
+~proof:(List.map formula_from_string [
+    "(x1 =>.((x1 =>. x1)=>.x1)) =>.
+    (( x1 =>. (x1 =>. x1)) =>. (x1 =>. x1))";
+    "x1=>.((x1=>.x1)=>.x1)";
+    "(x1 =>. (x1 =>. x1)) =>. (x1 =>. x1)";
+    "x1=>.(x1=>.x1)";
+    "x1=>.x1"
 ]);;
 
 (* |- (F=>.G)=>.(G=>.H)=>.(F=>.H)*)
-let verif_cut = (proof_verification ~hyp:[] ((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3)))
-~proof:[
-	(x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3));
-	((x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3)))=>.((x2=>.x3)=>.((x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3))));
-	((x2=>.x3)=>.((x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3))));
-	((x2=>.x3)=>.((x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3))))=>.(((x2=>.x3)=>.(x1=>.(x2=>.x3)))=>.((x2=>.x3)=>.((x1=>.x2)=>.(x1=>.x3))));
-	(((x2=>.x3)=>.(x1=>.(x2=>.x3)))=>.((x2=>.x3)=>.((x1=>.x2)=>.(x1=>.x3))));
-	((x2=>.x3)=>. (x1=>.(x2=>.x3)));
-	((x2=>.x3)=>.((x1=>.x2)=>.(x1=>.x3)));
-	((x2=>.x3)=>.((x1=>.x2)=>.(x1=>.x3)))=>.(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3)));
-	(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3)));
-	(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3)))=>.((x1=>.x2)=>.(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3))));	
-	((x1=>.x2)=>.(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3))));
-                        
-	(*k*)
-	((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x2)));
-	
-	(*s*)
-	((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3)))) =>.
-	(((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x2)))=>.((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3))));
-	
-	((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x2)))=>.((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3)));
-	((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3)))
+let verif_cut = proof_verification ~hyp:[] (formula_from_string "(x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3))")
+~proof: (List.map formula_from_string [
+    "(x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3))";
+    "((x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3)))=>.((x2=>.x3)=>.((x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3))))";
+    "((x2=>.x3)=>.((x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3))))";
+    "((x2=>.x3)=>.((x1=>.(x2=>.x3))=>.((x1=>.x2)=>.(x1=>.x3))))=>.(((x2=>.x3)=>.(x1=>.(x2=>.x3)))=>.((x2=>.x3)=>.((x1=>.x2)=>.(x1=>.x3))))";
+    "(((x2=>.x3)=>.(x1=>.(x2=>.x3)))=>.((x2=>.x3)=>.((x1=>.x2)=>.(x1=>.x3))))";
+    "((x2=>.x3)=>. (x1=>.(x2=>.x3)))";
+    "((x2=>.x3)=>.((x1=>.x2)=>.(x1=>.x3)))";
+    "((x2=>.x3)=>.((x1=>.x2)=>.(x1=>.x3)))=>.(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3)))";
+    "(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3)))";
+    "(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3)))=>.((x1=>.x2)=>.(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3))))";    
+    "((x1=>.x2)=>.(((x2=>.x3)=>.(x1=>.x2))=>.((x2=>.x3)=>.(x1=>.x3))))";
+    
+    (*k*)
+    "((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x2)))";
+    
+    (*s*)
+    "((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3)))) =>.
+    (((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x2)))=>.((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3))))";
+    
+    "((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x2)))=>.((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3)))";
+    "((x1=>.x2)=>.((x2=>.x3)=>.(x1=>.x3)))"
 ]);;
 
 (*non A =>. non B |- B =>. A*)
 let verif_contraposee = 
-        let h = ((neg (neg x2))=>.(neg (neg x1)))	
+        (*TODO delete when not needed anymore
+        let h = ((neg (neg x2))=>.(neg (neg x1)))    
         and a_ou_b =((neg (neg x1))=>. x1)
-        and i = ((neg (neg x2))=>. x1)	
-        and a2=	(x2=>.neg (neg x2))
+        and i = ((neg (neg x2))=>. x1)    
+        and a2=    (x2=>.neg (neg x2))
         in
-(proof_verification ~hyp:[] (((neg x1)=>.(neg x2))=>.(x2=>.x1))
-~proof:[
-
-a_ou_b;
-a_ou_b=>.(h=>.a_ou_b);
-h=>.a_ou_b;
-h =>.(a_ou_b=>.i);
-(h =>.(a_ou_b=>.i))=>.((h=>.a_ou_b)=>.(h=>.i));
-((h=>.a_ou_b)=>.(h=>.i));
-h=>.i;
-a2;
-a2=>.(h=>.a2);
-h=>.a2;
-a2=>.(i=>.(x2=>.x1));
-(a2=>.(i=>.(x2=>.x1)))=>. (h=>.(a2=>.(i=>.(x2=>.x1))));
-(h=>.(a2=>.(i=>.(x2=>.x1))));
-(h=>.(a2=>.(i=>.(x2=>.x1)))) =>. ((h=>.a2)=>.(h=>.(i=>.(x2=>.x1))));
-(h=>.a2)=>.(h=>.(i=>.(x2=>.x1)));
-h=>.(i=>.(x2=>.x1));
-(h=>.(i=>.(x2=>.x1)))=>.((h=>.i)=>.(h=>.(x2=>.x1)));
-(h=>.i)=>.(h=>.(x2=>.x1));
-h=>.(x2=>.x1);
-(((neg x1)=>.(neg x2))=>. h);
-(((neg x1)=>.(neg x2))=>. h)=>.((h=>.(x2=>.x1))=>.(((neg x1)=>.(neg x2))=>.(x2=>.x1)));
-((h=>.(x2=>.x1))=>.(((neg x1)=>.(neg x2))=>.(x2=>.x1)));
-(((neg x1)=>.(neg x2))=>.(x2=>.x1));
+        *)
+proof_verification ~hyp:[] (formula_from_string "(((neg x1)=>.(neg x2))=>.(x2=>.x1))")
+~proof:(List.map formula_from_string [
+    "a_ou_b";
+    "a_ou_b=>.(h=>.a_ou_b)";
+    "h=>.a_ou_b";
+    "h =>.(a_ou_b=>.i)";
+    "(h =>.(a_ou_b=>.i))=>.((h=>.a_ou_b)=>.(h=>.i))";
+    "((h=>.a_ou_b)=>.(h=>.i))";
+    "h=>.i";
+    "a2";
+    "a2=>.(h=>.a2)";
+    "h=>.a2";
+    "a2=>.(i=>.(x2=>.x1))";
+    "(a2=>.(i=>.(x2=>.x1)))=>. (h=>.(a2=>.(i=>.(x2=>.x1))))";
+    "(h=>.(a2=>.(i=>.(x2=>.x1))))";
+    "(h=>.(a2=>.(i=>.(x2=>.x1)))) =>. ((h=>.a2)=>.(h=>.(i=>.(x2=>.x1))))";
+    "(h=>.a2)=>.(h=>.(i=>.(x2=>.x1)))";
+    "h=>.(i=>.(x2=>.x1))";
+    "(h=>.(i=>.(x2=>.x1)))=>.((h=>.i)=>.(h=>.(x2=>.x1)))";
+    "(h=>.i)=>.(h=>.(x2=>.x1))";
+    "h=>.(x2=>.x1)";
+    "(((neg x1)=>.(neg x2))=>. h)";
+    "(((neg x1)=>.(neg x2))=>. h)=>.((h=>.(x2=>.x1))=>.(((neg x1)=>.(neg x2))=>.(x2=>.x1)))";
+    "((h=>.(x2=>.x1))=>.(((neg x1)=>.(neg x2))=>.(x2=>.x1)))";
+    "(((neg x1)=>.(neg x2))=>.(x2=>.x1))";
 ]);;
 
 (*|- A ou neg A*)
 let verif_tiers_exclus =
-let z = x1 ||. neg x1
+(*let z = x1 ||. neg x1
 and tout = neg (x1=>.x1)
 in
-(proof_verification ~hyp:[] (z)
-~proof:[
-
-(x1=>.x1);(**)
-(x1=>.x1) =>. (neg tout); (**)
-neg tout;(*OK*)
- 
-(neg z)=>.(neg z);(*OK*)
-
-x1=>.z;(**)
-(x1=>.z)=>.((neg z) =>. (x1=>.z));(**)
-(neg z)=>.(x1=>.(z));(*OK*)
-
-((x1=>.z)=>.((neg z)=>.(neg x1))); (**)
-((x1=>.z)=>.((neg z)=>.(neg x1))) =>. ((neg z)=>. ((x1=>.z)=>.((neg z)=>.(neg x1)))); (**)
-(neg z)=>.((x1=>.z)=>.((neg z)=>.(neg x1))); (*OK*)
-
-(x1=>.z)=>.((neg z) =>. neg x1);(**)
-((neg z) =>. neg x1);(**)
-((neg z) =>. neg x1)=>. ((neg z)=>.((neg z) =>. neg x1));(**)
-(neg z)=>.((neg z)=>.(neg x1));(*OK*)
-
-((neg z)=>.((neg z)=>.(neg x1)))=>.(((neg z)=>.(neg z))=>.((neg z)=>.(neg x1)));(**)
-((neg z)=>.(neg z))=>.((neg z)=>.(neg x1));(**)
-(neg z)=>.(neg x1);(*OK*)
-
-(neg x1)=>. z;(**)
-((neg x1)=>. z)=>. ((neg z)=>.((neg x1)=>. z));(**)
-(neg z)=>.((neg x1)=>.(z));(*OK*)
-((neg z)=>.((neg x1)=>.(z)))=>.(((neg z)=>. (neg x1))=>. ((neg z)=>. z));(**)
-((neg z)=>. (neg x1))=>. ((neg z)=>. z);(**)
-(neg z)=>.(z);(*OK*)
-
-(neg z)=>.((neg tout)=>.(neg z));(*OK*)
-
-((neg tout)=>.(neg z))=>.((z)=>.(tout));(**)
-(((neg tout)=>.(neg z))=>.((z)=>.(tout)))=>.
-((neg z)=>. (((neg tout)=>.(neg z))=>.((z)=>.(tout))));(**)
-
-(neg z)=>.(((neg tout)=>.(neg z))=>.((z)=>.(tout)));(*OK*)
-((neg z)=>.(((neg tout)=>.(neg z))=>.((z)=>.(tout))))=>.
-(((neg z)=>.((neg tout)=>.(neg z)))=>.((neg z)=>.(z=>.tout)));(**)
-(((neg z)=>.((neg tout)=>.(neg z)))=>.((neg z)=>.(z=>.tout)));(**)
-(neg z)=>.((z)=>.(tout));(*OK*)
-((neg z)=>.((z)=>.(tout)))=>.
-(((neg z)=>.z) =>. ((neg z) =>. tout));(**)
-(((neg z)=>.z) =>. ((neg z) =>. tout));(**)
-(neg z)=>.(tout);(*OK*)
-((neg z)=>.(tout))=>.((neg tout)=>.(neg(neg z)));(*OK*)
-(neg tout)=>.(neg(neg z));(*OK*)
-(neg(neg z))=>.(z);(*OK*)
-neg(neg z);(*OK*)
-z
+*)
+proof_verification ~hyp:[] (formula_from_string "x1 ||. neg x1")
+~proof:(List.map formula_from_string [
+    
+    "(x1=>.x1)";(**)
+    "(x1=>.x1) =>. (neg tout)"; (**)
+    "neg tout";(*OK*)
+     
+    "(neg z)=>.(neg z)";(*OK*)
+    
+    "x1=>.z";(**)
+    "(x1=>.z)=>.((neg z) =>. (x1=>.z))";(**)
+    "(neg z)=>.(x1=>.(z))";(*OK*)
+    
+    "((x1=>.z)=>.((neg z)=>.(neg x1)))"; (**)
+    "((x1=>.z)=>.((neg z)=>.(neg x1))) =>. ((neg z)=>. ((x1=>.z)=>.((neg z)=>.(neg x1))))"; (**)
+    "(neg z)=>.((x1=>.z)=>.((neg z)=>.(neg x1)))"; (*OK*)
+    
+    "(x1=>.z)=>.((neg z) =>. neg x1)";(**)
+    "((neg z) =>. neg x1)";(**)
+    "((neg z) =>. neg x1)=>. ((neg z)=>.((neg z) =>. neg x1))";(**)
+    "(neg z)=>.((neg z)=>.(neg x1))";(*OK*)
+    
+    "((neg z)=>.((neg z)=>.(neg x1)))=>.(((neg z)=>.(neg z))=>.((neg z)=>.(neg x1)))";(**)
+    "((neg z)=>.(neg z))=>.((neg z)=>.(neg x1))";(**)
+    "(neg z)=>.(neg x1)";(*OK*)
+    
+    "(neg x1)=>. z";(**)
+    "((neg x1)=>. z)=>. ((neg z)=>.((neg x1)=>. z))";(**)
+    "(neg z)=>.((neg x1)=>.(z))";(*OK*)
+    "((neg z)=>.((neg x1)=>.(z)))=>.(((neg z)=>. (neg x1))=>. ((neg z)=>. z))";(**)
+    "((neg z)=>. (neg x1))=>. ((neg z)=>. z)";(**)
+    "(neg z)=>.(z)";(*OK*)
+    
+    "(neg z)=>.((neg tout)=>.(neg z))";(*OK*)
+    
+    "((neg tout)=>.(neg z))=>.((z)=>.(tout))";(**)
+    "(((neg tout)=>.(neg z))=>.((z)=>.(tout)))=>.
+    ((neg z)=>. (((neg tout)=>.(neg z))=>.((z)=>.(tout))))";(**)
+    
+    "(neg z)=>.(((neg tout)=>.(neg z))=>.((z)=>.(tout)))";(*OK*)
+    "((neg z)=>.(((neg tout)=>.(neg z))=>.((z)=>.(tout))))=>.
+    (((neg z)=>.((neg tout)=>.(neg z)))=>.((neg z)=>.(z=>.tout)))";(**)
+    "(((neg z)=>.((neg tout)=>.(neg z)))=>.((neg z)=>.(z=>.tout)))";(**)
+    "(neg z)=>.((z)=>.(tout))";(*OK*)
+    "((neg z)=>.((z)=>.(tout)))=>.
+    (((neg z)=>.z) =>. ((neg z) =>. tout))";(**)
+    "(((neg z)=>.z) =>. ((neg z) =>. tout))";(**)
+    "(neg z)=>.(tout)";(*OK*)
+    "((neg z)=>.(tout))=>.((neg tout)=>.(neg(neg z)))";(*OK*)
+    "(neg tout)=>.(neg(neg z))";(*OK*)
+    "(neg(neg z))=>.(z)";(*OK*)
+    "neg(neg z)";(*OK*)
+    "z"
 ]);;
 
 (* |- (A=>.C)=>.(A=>.(B=>.C))*)
 let verif_rajout_hypothese =
-        let 
+        (*TODO to delete when not needed anymore
+         let 
         a,b,c=x1,x2,x3
         in
         let a_entraine_c = (a=>.c)
         and b_entraine_c = (b=>.c)
-        in
-(proof_verification ~hyp:[] (a_entraine_c=>.(a=>.b_entraine_c))
-~proof:[
- (*((S (K (S (K K)))) I)*)	
-a_entraine_c=>.a_entraine_c;(*i*)
-c=>.b_entraine_c; (*k*)
-(c=>.b_entraine_c)=>.(a=>.(c=>.b_entraine_c)); 
-(a=>.(c=>.b_entraine_c)); (*k k*)
-(a=>.(c=>.b_entraine_c))=>.
-(a_entraine_c=>.(a=>.b_entraine_c)); 
-(a_entraine_c=>.(a=>.b_entraine_c)); (*s (k k)*)
-(a_entraine_c=>.(a=>.b_entraine_c))=>.(a_entraine_c=>.(a_entraine_c=>.(a=>.b_entraine_c)));
-(a_entraine_c=>.(a_entraine_c=>.(a=>.b_entraine_c))); (*k (s (k k))*)
-(a_entraine_c=>.(a_entraine_c=>.(a=>.b_entraine_c)))=>.
-((a_entraine_c=>.a_entraine_c)=>.(a_entraine_c=>.(a=>.b_entraine_c)));
-((a_entraine_c=>.a_entraine_c)=>.(a_entraine_c=>.(a=>.b_entraine_c)));(*s (k (s (k k)))*)
-(a_entraine_c=>.(a=>.b_entraine_c)); (*(s (k (s (k k)))) i*)
+        in*)
+proof_verification ~hyp:[] (formula_from_string "a_entraine_c=>.(a=>.b_entraine_c)")
+~proof:(List.map formula_from_string [
+ (*((S (K (S (K K)))) I)*)    
+    "a_entraine_c=>.a_entraine_c";(*i*)
+    "c=>.b_entraine_c"; (*k*)
+    "(c=>.b_entraine_c)=>.(a=>.(c=>.b_entraine_c))"; 
+    "(a=>.(c=>.b_entraine_c))"; (*k k*)
+    "(a=>.(c=>.b_entraine_c))=>.
+    (a_entraine_c=>.(a=>.b_entraine_c))"; 
+    "(a_entraine_c=>.(a=>.b_entraine_c))"; (*s (k k)*)
+    "(a_entraine_c=>.(a=>.b_entraine_c))=>.(a_entraine_c=>.(a_entraine_c=>.(a=>.b_entraine_c)))";
+    "(a_entraine_c=>.(a_entraine_c=>.(a=>.b_entraine_c)))"; (*k (s (k k))*)
+    "(a_entraine_c=>.(a_entraine_c=>.(a=>.b_entraine_c)))=>.
+    ((a_entraine_c=>.a_entraine_c)=>.(a_entraine_c=>.(a=>.b_entraine_c)))";
+    "((a_entraine_c=>.a_entraine_c)=>.(a_entraine_c=>.(a=>.b_entraine_c)))";(*s (k (s (k k)))*)
+    "(a_entraine_c=>.(a=>.b_entraine_c))"; (*(s (k (s (k k)))) i*)
 ]);;
 
 (* |- F ou F =>. F *)
 let verif_ou_idempotent =
-(proof_verification ~hyp:[] ((x1 ||. x1)=>.x1)
-~proof:[
-((x1 ||.x1) =>. x1)=>.((neg x1) =>. neg (x1||.x1));
-((neg x1)=>. ((x1 ||. x1) =>. x1));
-((neg x1)=>. ((x1 ||. x1) =>. x1))=>.((((x1 ||.x1) =>. x1)=>.((neg x1) =>. neg (x1||.x1)))=>.((neg x1)=>.((neg x1) =>. neg (x1||.x1))));
-((((x1 ||.x1) =>. x1)=>.((neg x1) =>. neg (x1||.x1)))=>.((neg x1)=>.((neg x1) =>. neg (x1||.x1))));
-((neg x1)=>.((neg x1) =>. neg (x1||.x1)));
-((neg x1)=>.((neg x1) =>. neg (x1||.x1)))=>. (((neg x1) =>. (neg x1))=>.((neg x1)=>.(neg (x1||.x1))));
-((neg x1) =>. (neg x1));
-(((neg x1) =>. (neg x1))=>.((neg x1)=>.(neg (x1||.x1))));
-((neg x1)=>.(neg (x1||.x1)));
-((neg x1)=>.(neg (x1||.x1)))=>. ((x1||.x1) =>.x1);
-(x1||.x1) =>. x1;
+proof_verification ~hyp:[] (formula_from_string "(x1 ||. x1)=>.x1")
+~proof:(List.map formula_from_string [
+    "((x1 ||.x1) =>. x1)=>.((neg x1) =>. neg (x1||.x1))";
+    "((neg x1)=>. ((x1 ||. x1) =>. x1))";
+    "((neg x1)=>. ((x1 ||. x1) =>. x1))=>.((((x1 ||.x1) =>. x1)=>.((neg x1) =>. neg (x1||.x1)))=>.((neg x1)=>.((neg x1) =>. neg (x1||.x1))))";
+    "((((x1 ||.x1) =>. x1)=>.((neg x1) =>. neg (x1||.x1)))=>.((neg x1)=>.((neg x1) =>. neg (x1||.x1))))";
+    "((neg x1)=>.((neg x1) =>. neg (x1||.x1)))";
+    "((neg x1)=>.((neg x1) =>. neg (x1||.x1)))=>. (((neg x1) =>. (neg x1))=>.((neg x1)=>.(neg (x1||.x1))))";
+    "((neg x1) =>. (neg x1))";
+    "(((neg x1) =>. (neg x1))=>.((neg x1)=>.(neg (x1||.x1))))";
+    "((neg x1)=>.(neg (x1||.x1)))";
+    "((neg x1)=>.(neg (x1||.x1)))=>. ((x1||.x1) =>.x1)";
+    "(x1||.x1) =>. x1";
 ]);;
 (*TODO 
 (* |- (A ou B)=>.(A=>.C)=>.(B=>.C)=>.C*)
@@ -529,7 +533,7 @@ a_ou_b=>.
 (a_entraine_c=>.((b_entraine_c=>.(neg tout))=>.(b_entraine_c=>.(neg(neg (c))))))=>.
 ((a_entraine_c=>.(b_entraine_c=>.(neg tout)))=>.(a_entraine_c=>.(b_entraine_c=>.(neg(neg (c))))));
 
-(a_ou_b=>.(a_entraine_c=>.((b_entraine_c=>.(neg tout))=>.(b_entraine_c=>.(neg(neg (c)))))))=>.(((a_entraine_c=>.((b_entraine_c=>.(neg tout))=>.(b_entraine_c=>.(neg(neg (c))))))=>.((a_entraine_c=>.(b_entraine_c=>.(neg tout)))=>.(a_entraine_c=>.(b_entraine_c=>.(neg(neg (c)))))))=>.(a_ou_b=>.((a_entraine_c=>.(b_entraine_c=>.(neg tout)))=>.(a_entraine_c=>.(b_entraine_c=>.(neg(neg (c))))))));	         
+(a_ou_b=>.(a_entraine_c=>.((b_entraine_c=>.(neg tout))=>.(b_entraine_c=>.(neg(neg (c)))))))=>.(((a_entraine_c=>.((b_entraine_c=>.(neg tout))=>.(b_entraine_c=>.(neg(neg (c))))))=>.((a_entraine_c=>.(b_entraine_c=>.(neg tout)))=>.(a_entraine_c=>.(b_entraine_c=>.(neg(neg (c)))))))=>.(a_ou_b=>.((a_entraine_c=>.(b_entraine_c=>.(neg tout)))=>.(a_entraine_c=>.(b_entraine_c=>.(neg(neg (c))))))));             
 (((a_entraine_c=>.((b_entraine_c=>.(neg tout))=>.(b_entraine_c=>.(neg(neg (c))))))=>.((a_entraine_c=>.(b_entraine_c=>.(neg tout)))=>.(a_entraine_c=>.(b_entraine_c=>.(neg(neg (c)))))))=>.(a_ou_b=>.((a_entraine_c=>.(b_entraine_c=>.(neg tout)))=>.(a_entraine_c=>.(b_entraine_c=>.(neg(neg (c))))))));
 a_ou_b=>.((a_entraine_c=>.(b_entraine_c=>.(neg tout)))=>.(a_entraine_c=>.(b_entraine_c=>.(neg(neg (c))))));
 
@@ -585,7 +589,7 @@ let test_ou_idempotent test_ctxt = assert_bool "ou idempotent" (verif_ou_idempot
 
 let x1,x2,x3 = PVar 1, PVar 2, PVar 3
 
-let test_instance test_ctxt = assert_bool "instance" (instance (x1 &&. x2) (x1 &&. x2))
+let test_instance test_ctxt = assert_bool "instance" (instance (formula_from_string "x1 &&. x2") (formula_from_string "x1 &&. x2"))
 
 (** Tests for to_string *)
 let test_to_string_formula_pvar test_ctxt =
@@ -593,31 +597,31 @@ let s = to_string_formula_prop  (x1)
 in assert_equal s "P1"
                                                 
 let test_to_string_formula_pneg test_ctxt =
-let s = to_string_formula_prop  (neg x1)
+let s = to_string_formula_prop  (formula_from_string "neg x1")
 in assert_equal s "!P1"
 
 let test_to_string_formula_pand test_ctxt =
-let s =  to_string_formula_prop  (x1 &&. x2)
+let s =  to_string_formula_prop  (formula_from_string "x1 &&. x2")
 in assert_equal s "P1 /\\ P2"
 
 let test_to_string_formula_por test_ctxt = 
-let s = to_string_formula_prop  (x1 ||. x2)
+let s = to_string_formula_prop  (formula_from_string "x1 ||. x2")
 in assert_equal s "P1 \\/ P2"
 
 let test_to_string_formula_pand_por test_ctxt = 
-let s = to_string_formula_prop  ((x1 &&. x2) ||. x3)
+let s = to_string_formula_prop  (formula_from_string "(x1 &&. x2) ||. x3")
 in assert_equal s "(P1 /\\ P2) \\/ P3"
 
 let test_to_string_formula_por_pand test_ctxt = 
-let s = to_string_formula_prop  ((x1 ||. x2) &&. x3)
+let s = to_string_formula_prop  (formula_from_string "(x1 ||. x2) &&. x3")
 in assert_equal s "(P1 \\/ P2) /\\ P3"
 
 let test_to_string_formula_impl test_ctxt = 
-let s = to_string_formula_prop  (x1 =>. x2)
+let s = to_string_formula_prop  (formula_from_string "x1 =>. x2")
 in assert_equal s "P1 => P2"
 
 let test_to_string_formula_and_impl test_ctxt = 
-let s = to_string_formula_prop  (x3 &&. (x1 =>. x2))
+let s = to_string_formula_prop  (formula_from_string "x3 &&. (x1 =>. x2)")
 in assert_equal s "P3 /\\ (P1 => P2)"
 
 (* Tests for printer_formula *)
@@ -625,31 +629,31 @@ let test_printer_formula_pvar test_ctxt = printer_formula_prop Format.str_format
 let s = Format.flush_str_formatter()
 in assert_equal s "P1"
                                                 
-let test_printer_formula_pneg test_ctxt = printer_formula_prop Format.str_formatter (neg x1);
+let test_printer_formula_pneg test_ctxt = printer_formula_prop Format.str_formatter (formula_from_string "neg x1");
 let s = Format.flush_str_formatter()
 in assert_equal s "!P1"
 
-let test_printer_formula_pand test_ctxt = printer_formula_prop Format.str_formatter (x1 &&. x2);
+let test_printer_formula_pand test_ctxt = printer_formula_prop Format.str_formatter (formula_from_string "x1 &&. x2");
 let s = Format.flush_str_formatter()
 in assert_equal s "P1 /\\ P2"
 
-let test_printer_formula_por test_ctxt = printer_formula_prop Format.str_formatter (x1 ||. x2);
+let test_printer_formula_por test_ctxt = printer_formula_prop Format.str_formatter (formula_from_string "x1 ||. x2");
 let s = Format.flush_str_formatter()
 in assert_equal s "P1 \\/ P2"
 
-let test_printer_formula_pand_por test_ctxt = printer_formula_prop Format.str_formatter ((x1 &&. x2) ||. x3);
+let test_printer_formula_pand_por test_ctxt = printer_formula_prop Format.str_formatter (formula_from_string "(x1 &&. x2) ||. x3");
 let s = Format.flush_str_formatter()
 in assert_equal s "(P1 /\\ P2) \\/ P3"
 
-let test_printer_formula_por_pand test_ctxt = printer_formula_prop Format.str_formatter ((x1 ||. x2) &&. x3);
+let test_printer_formula_por_pand test_ctxt = printer_formula_prop Format.str_formatter (formula_from_string "(x1 ||. x2) &&. x3");
 let s = Format.flush_str_formatter()
 in assert_equal s "(P1 \\/ P2) /\\ P3"
 
-let test_printer_formula_impl test_ctxt = printer_formula_prop Format.str_formatter (x1 =>. x2);
+let test_printer_formula_impl test_ctxt = printer_formula_prop Format.str_formatter (formula_from_string "x1 =>. x2");
 let s = Format.flush_str_formatter()
 in assert_equal s "P1 => P2"
 
-let test_printer_formula_and_impl test_ctxt = printer_formula_prop Format.str_formatter (x3 &&. (x1 =>. x2));
+let test_printer_formula_and_impl test_ctxt = printer_formula_prop Format.str_formatter (formula_from_string "x3 &&. (x1 =>. x2)");
 let s = Format.flush_str_formatter()
 in assert_equal s "P3 /\\ (P1 => P2)"
 
