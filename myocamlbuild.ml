6b4d8803438a1ae910c6c1e307b552c1c83517e2
S K <sylvain_kerjean@hotmail.com>
2016-05-22 17:43:58 +0200

    Adding optionsgenerate the .mli from parser (mandatory to make the parser a functor)

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -6,7 +6,9 @@ dispatch begin function
       ~deps:["%.dyp"]
       begin fun env _ ->
         let dyp = env "%.dyp" in
-        Cmd(S[A"dypgen"; A"--no-mli"; Px dyp])
+(*        Cmd(S[A"dypgen"; A"--no-mli"; Px dyp])
+ *)
+          Cmd(S[A"dypgen"; A"--ocamlc"; A "-I `ocamlfind query dyp`";Px dyp])
       end;
   | _ ->  ()
 end
