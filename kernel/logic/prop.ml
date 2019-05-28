95fe1695427f095164142c05244baa95e789aa23
Sylvain78 <sylvain_kerjean@hotmail.com>
2019-05-28 05:40:21 +0000

    axioms

--- kernel/logic/prop.ml
+++ kernel/logic/prop.ml
@@ -13,33 +13,42 @@ module Formula_Prop = struct
 
   let of_string _ = PVar 0
   let to_string _ = ""
-
-
-  let c1 = function PVar _ | PAnd _ |POr _ | PImpl _ | _ -> 0;; 
+  let c1 = function PVar _ | PAnd _ | POr _ | PImpl _ | _ -> 0
   let a = PVar 0
-  let b = PAnd(a,a)
-  let c = POr(b,b)
-  let d = PImpl(c,c);;
-  ignore (d,c1)
+  let b = PAnd (a, a)
+  let c = POr (b, b)
+  let d = PImpl (c, c);;
 
+  ignore (d, c1)
 end
 
 module Prop : LOGIC with type formula = Formula_Prop.formula = struct
   let name = "Prop"
-  module L = Formula_Prop
-  type formula = L.formula
-  type rule =  formula list -> formula
-  let cut = function 
-    |[f;L.PImpl(f',g)] when f=f' -> g
-    | _ -> failwith "cut inapplicable"
-  
-  let rules = [ cut]
- (*TODO regle coupure*)
-  let a1 = L.of_string "X_1 \\implies (X_2 \\implies X_1)"
-  let axiom f = f = a1
-  let heuristic_proof ?(premisses = empty_family) formula = ignore (premisses,formula);None
 
-  type demonstration = { hypotheses : formula family ; demonstration : formula list }
+  module L = Formula_Prop
+
+  type formula = L.formula
+  type rule = formula list -> formula
+
+  let cut = function
+    | [ f; L.PImpl (f', g) ] when f = f' -> g
+    | _ -> failwith "cut inapplicable"
+
+  let rules = [ cut ]
+
+  (*TODO regle coupure*)
+  let a1 = L.of_string "X_1 \\implies (X_2 \\implies X_1)"
+  let a2 = L.of_string "(X_1 \\implies (X_2 \\implies X_3)) \\implies ((X_1 \\implies X_2) \\implies (X_1 \\implies X_3))"
+  let axiom f = List.mem f [a1 ; a2]
+
+  let heuristic_proof ?(premisses = empty_family) formula =
+    ignore (premisses, formula);
+    None
+
+  type demonstration = {
+    hypotheses : formula family;
+    demonstration : formula list;
+  }
 
   let verification _ = false
 end
