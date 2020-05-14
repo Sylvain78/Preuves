281ab165a0f15fe28cb29e8fe0edb16ee8755309
shredder <sylvain_kerjean@hotmail.com>
2020-05-15 01:59:53 +0200

    some cleaning

--- util/util.ml
+++ util/util.ml
@@ -1,16 +1,9 @@
 let new_var =
-	let v = ref 0
-	in
-	function () ->
-			begin
-				incr v;
-				if (!v <0) then failwith "plus de variables libres dispo";
-				!v
-			end;;
-
-
-let rec print_liste ff f = function
-	| [] -> ()
-	| [a] -> f ff a
-	| a:: l -> f ff a; Format.fprintf ff (", "); print_liste ff f l
-
+  let v = ref 0
+  in
+  function () ->
+    begin
+      incr v;
+      if (!v <0) then failwith "No more free variables available. You exhaust me !!";
+      !v
+    end;;
