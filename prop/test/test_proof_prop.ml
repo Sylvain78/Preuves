6b6b86bda222e66e5abc4c9630b3de596eaa4df6
shredder <sylvain_kerjean@hotmail.com>
2020-12-13 02:29:05 +0100

    unused parameter

--- prop/test/test_proof_prop.ml
+++ prop/test/test_proof_prop.ml
@@ -4,19 +4,19 @@ open Proof_prop
 
 let x1,x2 = PVar 1,PVar 2
 
-let test_invalid_empty_proof test_ctxt =
+let test_invalid_empty_proof _ =
         assert_raises ~msg:"Invalid empty proof" (Failure("Formula is not at the end of the proof")) (fun () -> prop_proof_kernel_verif ~hyp:[] ~proof:[] x1)
 
-let test_invalid_proof test_ctxt =
+let test_invalid_proof _ =
   assert_raises ~msg:"Invalid proof" (Invalid_demonstration (x1,[x1])) (fun () -> prop_proof_kernel_verif ~hyp:[] ~proof:[(*TPPFormula*) x1] x1)
 
-let test_invalid_proof_length_2 test_ctxt =
+let test_invalid_proof_length_2 _ =
   assert_raises ~msg:"Invalid proof" (Invalid_demonstration (x1,[x1])) (fun () -> prop_proof_kernel_verif ~hyp:[] ~proof:[(*TPPFormula*) x1;(*TPPFormula*) x2] x2)
 
-let test_invalid_proof_length_2_hyp test_ctxt =
+let test_invalid_proof_length_2_hyp _ =
         assert_raises ~msg:"Invalid proof" (Invalid_demonstration (x2,[x1;x2])) (fun () -> prop_proof_kernel_verif ~hyp:[x1] ~proof:[(*TPPFormula*) x1;(*TPPFormula*) x2] x2)
 
-let test_S1_Bourbaki test_ctxt =
+let test_S1_Bourbaki _ =
   (*let _ = notation_from_string "Notation\nimply\nParam\na b\nSyntax\na \"\\implies\" b\nSemantics\na \" \\implies \" b\nEnd"
   in*)
   let f = formula_from_string "((\\mathbf{A} \\lor \\mathbf{A})  \\implies  \\mathbf{A})  \\implies  ((\\lnot \\mathbf{A})  \\implies  \\lnot (\\mathbf{A} \\lor \\mathbf{A}))"
