0805813dc0270b79d1da72c49b9ff814b9a8ebf3
Anarchos <sylvain_kerjean@hotmail.com>
2021-09-10 10:13:48 +0000

    implementation (useless)

---
+++ prop/formula_prop.ml
@@ -0,0 +1,32 @@
+type notation_prop_element = Param of string | String of string
+
+type formula_prop =
+  | PVar of int
+  | PMetaVar of string
+  | PNeg of formula_prop
+  | PAnd of formula_prop * formula_prop
+  | POr of formula_prop * formula_prop
+  | PImpl of formula_prop * formula_prop
+  | PApply_notation of apply_notation_prop
+and apply_notation_prop =
+  {
+    apply_notation_prop : notation_prop;
+    apply_notation_prop_params : formula_prop list; (*SKE TODO create database of notations*)
+  }
+and notation_prop =
+  {
+    notation_prop_name : string;
+    notation_prop_params : notation_prop_element list;
+    notation_prop_notation : notation_prop_element list;
+    notation_prop_semantique : notation_prop_element list;
+  }  
+(*SKE Example of notation
+  "
+  Notation equiv
+  Params a b
+  a \equiv b 
+  (a \imply b)\\land(b \imply a)
+  End
+  "
+*)
+
