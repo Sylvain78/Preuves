710e72b7d3ef0f49b02a84827905c791a6aeb88a
Anarchos <sylvain_kerjean@hotmail.com>
2021-12-13 21:59:44 +0100

    adding S3 in test

--- kernel/kernel_prop/test/test_compile.ml
+++ kernel/kernel_prop/test/test_compile.ml
@@ -26,22 +26,52 @@ let test_compile _ =
 
 let test_compile_a_implies_a _ =
   assert_equal 
-    { theorem=formula ; 
-      demonstration= [Ax (2, [(3, x1); (1, x1); (2, x1 => x1)]);
-                      Ax (1, [(1, x1); (2, x1 => x1)]);
-                      Cut (2, 1);
-                      Ax (1, [(1, x1); (2, x1)]);
-                      Cut (4, 3)]}
+    { theorem = formula ; 
+      demonstration = [Ax (2, [(3, x1); (1, x1); (2, x1 => x1)]);
+                       Ax (1, [(1, x1); (2, x1 => x1)]);
+                       Cut (2, 1);
+                       Ax (1, [(1, x1); (2, x1)]);
+                       Cut (4, 3)
+                      ]
+    }
 
     (compile_demonstration ~demo:demo ())
 
+let test_instance_s1_3 _ =
+  let _ = Prop.Prop_parser.notation_from_string "Notation\nimply\nParam\na b\nSyntax\na \"=>\" b\nSemantics\n\"(\"a\")\" \"\\implies\" \"(\"b\")\"\nEnd" and 
+  f = Prop.Prop_parser.formula_from_string 
+      "((\\lnot \\mathbf{A}) \\implies ((\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A})) \\implies ((((\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A}) \\implies ((\\lnot \\mathbf{A}) \\implies \\lnot (\\mathbf{A} \\lor \\mathbf{A}))) \\implies ((\\lnot \\mathbf{A}) \\implies ((\\lnot \\mathbf{A}) \\implies \\lnot (\\mathbf{A} \\lor \\mathbf{A}))))";
+  in 
+  let subst = (Prop.Formula_tooling.instance f Prop.Axioms_prop.a2)
+  in
+  assert_equal true (List.length subst > 0)
+
+let test_compile_s1 _ =
+  let _ = Prop.Prop_parser.notation_from_string "Notation\nimply\nParam\na b\nSyntax\na \"=>\" b\nSemantics\n\"(\"a\")\" \"\\implies\" \"(\"b\")\"\nEnd" and 
+  s1 = Prop.Prop_parser.formula_from_string "(\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A}"
+  and demo = compile_demonstration ~demo:(List.map Prop.Prop_parser.formula_from_string [
+      "((\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A}) \\implies ((\\lnot \\mathbf{A}) \\implies \\lnot (\\mathbf{A} \\lor \\mathbf{A}))";
+      "((\\lnot \\mathbf{A}) \\implies ((\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A}))";
+      "((\\lnot \\mathbf{A}) \\implies ((\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A})) \\implies ((((\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A}) \\implies ((\\lnot \\mathbf{A}) \\implies \\lnot (\\mathbf{A} \\lor \\mathbf{A}))) \\implies ((\\lnot \\mathbf{A}) \\implies ((\\lnot \\mathbf{A}) \\implies \\lnot (\\mathbf{A} \\lor \\mathbf{A}))))";
+      "((((\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A}) \\implies ((\\lnot \\mathbf{A}) \\implies \\lnot (\\mathbf{A} \\lor \\mathbf{A}))) \\implies ((\\lnot \\mathbf{A}) \\implies ((\\lnot \\mathbf{A}) \\implies \\lnot (\\mathbf{A} \\lor \\mathbf{A}))))";
+      "((\\lnot \\mathbf{A}) \\implies ((\\lnot \\mathbf{A}) \\implies \\lnot (\\mathbf{A} \\lor \\mathbf{A})))";
+      "((\\lnot \\mathbf{A}) \\implies ((\\lnot \\mathbf{A}) \\implies \\lnot (\\mathbf{A} \\lor \\mathbf{A}))) \\implies (((\\lnot \\mathbf{A}) \\implies (\\lnot \\mathbf{A})) \\implies ((\\lnot \\mathbf{A}) \\implies (\\lnot (\\mathbf{A} \\lor \\mathbf{A}))))";
+      "((\\lnot \\mathbf{A}) \\implies (\\lnot \\mathbf{A}))";
+      "(((\\lnot \\mathbf{A}) \\implies (\\lnot \\mathbf{A})) \\implies ((\\lnot \\mathbf{A}) \\implies (\\lnot (\\mathbf{A} \\lor \\mathbf{A}))))";
+      "((\\lnot \\mathbf{A}) \\implies (\\lnot (\\mathbf{A} \\lor \\mathbf{A})))";
+      "((\\lnot \\mathbf{A}) \\implies (\\lnot (\\mathbf{A} \\lor \\mathbf{A}))) \\implies ((\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A})";
+      "(\\mathbf{A} \\lor \\mathbf{A}) \\implies \\mathbf{A}";
+    ]) ()
+  in
+  assert_equal { theorem = s1 ; demonstration=[]} demo
 let compile_suite =
   "compile test" >:::
   [ 
     "test cut" >:: test_cut;
     "test compile" >:: test_compile;
     "test_compile_a_implies_a" >:: test_compile_a_implies_a;
-  ]
+    "test_compile_s1" >:: test_compile_s1;
+    "test_instance_s1_3" >:: test_instance_s1_3]
 
 let () = 
   run_test_tt_main compile_suite
