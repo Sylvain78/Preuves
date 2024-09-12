f6bad4cae841d2f346ac4d4ab42d0db758b6df99
Sylvain78 <sylvain_kerjean@hotmail.com>
2024-09-12 19:52:46 +0000

    Warning for Invalidate

--- server/protocol.ml
+++ server/protocol.ml
@@ -12,6 +12,7 @@ type latex_output = LMath | LText
 type answer = 
   | Ok of Protocol_commands.command
   | Answer of output * (latex_output option) * string
+  | Warning of string
   | Error of string
   | Quit
 
@@ -27,6 +28,7 @@ let encode_answer = function
   | Answer (Latex, Some LMath, s) -> Bytes.(concat empty ( [of_string "Answer"; of_string "latex";of_string "math"; Protocol_commands.encode_string s]))
   | Answer (Latex, Some LText, s) -> Bytes.(concat empty ( [of_string "Answer"; of_string "latex"; of_string "text";  Protocol_commands.encode_string s]))
   | Answer (Text, None, s) -> Bytes.(concat empty ( [of_string "Answer"; of_string "text"; Protocol_commands.encode_string s]))
-  | Error s -> Bytes.(cat (of_string ("Error:")) (Protocol_commands.encode_string s))
+  | Warning s -> Bytes.(cat (of_string ("Warning")) (Protocol_commands.encode_string s))
+  | Error s -> Bytes.(cat (of_string ("Error")) (Protocol_commands.encode_string s))
   | Quit -> Bytes.(concat empty ( [of_string "Quit"]))
   | _ -> failwith "unimplemented2"
