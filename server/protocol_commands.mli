9ea40176a5c571e0b55a33e65d6a6c39124325b0
Sylvain78 <sylvain_kerjean@hotmail.com>
2024-09-25 14:25:45 +0000

    params in axiom + unknown command

--- server/protocol_commands.mli
+++ server/protocol_commands.mli
@@ -19,13 +19,14 @@ type command =
   | Load of Modes.ascii_mode * string
   | Notation of { name:string ; params: string list ; syntax : notation_element list ; semantics : notation_element list }
   | Theorem of {name:string ; params: string list; premisses: string list; conclusion : string ; demonstration : demonstration_step list; kind : Kernel.Logic.kind}
-  | Axiom of {name:string; formula:string} 
+  | Axiom of {name:string; params: string list; formula:string}
   | Invalidate of string
   | History
   | Show of string
   | List of [`Axioms | `Theorems | `Files]
   | User of string
   | Quit
+  | Unknown of string
 
 val encode_string : string -> bytes
 val encode_command : command -> bytes
